89146f567909f9005c305e3e6d61961943ef3c6f
aravindashokk <aravindkayal@gmail.com>
2024-03-01 14:15:05 -0600

    card css changes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,9 +3,9 @@ const ItemList = ({ items }) => {
         <div>
             {items.map((item) => (
                 <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">
-                    <div className="p-2">
-                        <span>{item.card.info?.name}</span>
-                        <span> - ₹ {item.card.info?.price}</span>
+                    <div className="py-2">
+                        <span>{item.card.info.name}</span>
+                        <span> - ₹ {item.card.info.price/100}</span>
                     </div>
                     <p className="text-xs">{item.card.info.description}</p>
                 </div>
